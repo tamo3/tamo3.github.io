45111cb6642add64d80a5d9fc07049be818905bc
tamo3 <tamotsu@sightlineapplications.com>
2020-01-26 23:32:10 -0800

    nav menu now uses JS so that it can be re-sued in multiple pages.

---
+++ nav.js
@@ -0,0 +1,9 @@
+var html =
+  '<ul>\
+<li><a href="index.html">Home</a></li>\
+<li><a href="1-photos.html">Photos</a></li>\
+<li><a href="2-music.html">Music</a></li>\
+<li><a href="3-tech.html">Technology</a></li>\
+</ul>';
+
+document.getElementById('nav').innerHTML = html;
