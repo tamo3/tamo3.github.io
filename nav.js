0b0823b4d5b412669f3e8737ba67cf9878cc078f
tamotsu tanabe <tamotsu@sightlineapplications.com>
2020-01-27 21:41:11 -0800

    Switching Bootstrap 4 based navigation bar.

--- nav.js
+++ nav.js
@@ -1,11 +1,20 @@
 // Based on: https://stackoverflow.com/questions/44502243/single-navigation-bar-across-website
 
-var html =
-  '<ul>\
-<li><a href="index.html">Home</a></li>\
-<li><a href="1-photos.html">Photos</a></li>\
-<li><a href="2-music.html">Music</a></li>\
-<li><a href="3-tech.html">Technology</a></li>\
+// var html =
+//   '<ul>\
+// <li><a href="index.html">Home</a></li>\
+// <li><a href="1-photos.html">Photos</a></li>\
+// <li><a href="2-music.html">Music</a></li>\
+// <li><a href="3-tech.html">Technology</a></li>\
+// </ul>';
+
+var html = '<ul class="navbar-nav">\
+<li class="nav-item active">  <a class="nav-link"  href="#">Home</a></li>\
+<li class="nav-item"><a class="nav-link"  href="2-photos.html">Photos</a></li>\
+<li class="nav-item"><a class="nav-link"  href="#">Music</a></li>\
+<li class="nav-item"><a class="nav-link"  href="#">Technology</a></li>\
 </ul>';
 
 document.getElementById('nav').innerHTML = html;
+
+
